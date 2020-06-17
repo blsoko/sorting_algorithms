cfd1810b1608e4c3c502b5fbda40c4aaa4892834
blsoko <exanymejose1@hotmail.com>
2020-06-17 10:39:05 -0500

    First exercise

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "/home/blsoko/HolbertonSchool/sorting_algorithms/sort.h"
+
+/**
+ * bubble_sort - Prints a list of integers
+ *
+ * @array: Unordered array
+ * @size: size of the array
+ *
+ * Return - Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp = 0, i, j;
+
+	for (i = size - 2; i >= 0; i--)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
